6c6f53eed9012d7f20aa87c842976155544358eb
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2017-03-27 17:34:25 -0700

    Coding challenge initial setup.

---
+++ CarAPIV1/src/test/java/com/equalinformation/carapi/java/test/Test3.java
@@ -0,0 +1,8 @@
+package com.equalinformation.carapi.java.test;
+
+/**
+ * Created by bpupadhyaya on 3/27/17.
+ */
+public class Test3 {
+    // TODO clean up later
+}
---
+++ CarAPIV1/src/test/scala/com/equalinformation/carapi/scala/test/Test4.scala
@@ -0,0 +1,9 @@
+package com.equalinformation.carapi.scala.test
+
+/**
+  * Created by bpupadhyaya on 3/27/17.
+  */
+class Test4 {
+  //TODO clean up later
+
+}
